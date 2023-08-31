d1520c4fb75a2c57d62f3013e88677e57f65741b
redwan <redtah2007@yahoo.com>
2023-08-31 05:35:12 -0700

    Fix eslint error

--- src/redux/store.js
+++ src/redux/store.js
@@ -2,9 +2,11 @@ import { configureStore } from '@reduxjs/toolkit';
 import booksReducer from './books/booksSlice';
 import categoriesReducer from './categories/categoriesSlice';
 
-export const store = configureStore({
+const store = configureStore({
   reducer: {
     books: booksReducer,
     category: categoriesReducer,
   },
 });
+
+export default store;
